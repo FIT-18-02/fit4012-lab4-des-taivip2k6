6fc41cbaf0918276172e8364f621e7722ea8ab61
taivip2k6 <taibonxu10001@gmail.com>
2026-05-07 22:36:16 +0700

    Update run_sample.sh

--- scripts/run_sample.sh
+++ scripts/run_sample.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Compiling des.cpp ..."
 g++ -std=c++17 -Wall -Wextra -pedantic des.cpp -o des
-
-echo "Build successful!"
-echo "Running program..."
-echo "-------------------------"
-
 ./des
